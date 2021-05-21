146f67b648ec5aec14b546828fa8c1346994332e
KevinAlmada <kevinalmada81@gmail.com>
2021-05-21 12:26:35 -0300

    nuevos autos

---
+++ concesionaria.js
@@ -0,0 +1,19 @@
+let autos =[{
+    marca:"Renault",
+    modelo:"Duster",
+    precio:800000,
+    km:30000,
+    color:"rojo",
+    anio:2013,
+    patente:"JJJ758",
+    vendido: false,
+},{
+    marca:"Lamborgoti",
+    modelo:"fasterosa",
+    precio:80000000,
+    km:0,
+    color:"rojo",
+    anio:2020,
+    patente:"ZZZ852",
+    vendido: false,
+}]
