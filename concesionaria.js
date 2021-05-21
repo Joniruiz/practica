530a8b69229649ef7673d75269da4f3c9288b720
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-05-21 12:14:11 -0300

    Agregue nuevos objetos en concesionarias

---
+++ concesionaria.js
@@ -0,0 +1,21 @@
+let autos =[ 
+    {
+    marca:"Ford",
+    modelo:"Fiesta",
+    precio:1650000,
+    km:30000,
+    color:"Gris",
+    anio:2019,
+    patente:"APP123",
+    vendido: false,
+},{
+    marca:"Peugeot",
+    modelo:"206",
+    precio:570000,
+    km:150000,
+    color:"Blanco",
+    anio:2007,
+    patente:"FVZ180",
+    vendido: false,
+},
+]
