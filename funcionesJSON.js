a73d9697fc24c7b4d1fd3fde15004d424ddb6734
Franckfer <francodel4to@gmail.com>
2021-05-21 13:45:19 -0300

    agregue escribirJson

--- funcionesJSON.js
+++ funcionesJSON.js
@@ -9,5 +9,20 @@ module.exports =  FuncionalidadesJSON={
          let autoJSON=JSON.stringify(autosActual);
          fs.writeFileSync('./datos.json', autoJSON, 'utf-8');
     },
-    
+    escribirJson : (marca, modelo, precio, km, color, anio, patente, vendido) => {
+        let nuevoAuto = {
+            marca: marca,
+            modelo: modelo,
+            precio: precio,
+            km: km,
+            color: color,
+            anio: anio,
+            patente: patente,
+            vendido: vendido,
+        } 
+        let autosStock = FuncionalidadesJSON.leerJSON();
+        autosStock.push(nuevoAuto);
+        FuncionalidadesJSON.guardarJSON(autosStock);
+        console.log("auto agregado");
+    }
 }
