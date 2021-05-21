a33e458363397a34e164b7cf5e7ee11f91cb9fd1
Kevin Ibañez <kevinkm2610@gmail.com>
2021-05-21 12:24:28 -0300

    hola soy kevin2

---
+++ concesionaria.js
@@ -0,0 +1,22 @@
+let autos = [
+    {
+        marca:"RAM",
+        modelo:"Larimie 1500",
+        precio:7490000,
+        km:0,
+        color:"Negro",
+        anio:2020,
+        patente:"RIT590",
+        vendido: false,
+    },
+    {
+        marca:"Citroën",
+        modelo:"C3",
+        precio:640000,
+        km:0,
+        color:"Blanco",
+        anio:2020,
+        patente:"KEV222",
+        vendido: false,
+    }
+]
