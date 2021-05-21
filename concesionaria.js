6a9070065128d79938ae50ec0e781e4dd1dbef02
Mauricio <alanmaneyro@hotmail.com>
2021-05-21 12:14:16 -0300

    mauri update

---
+++ concesionaria.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+[
+    {
+    marca:"Mauri",
+    modelo:"200x",
+    precio:1000000,
+    km:0,
+    color:"espejo",
+    anio:2019,
+    patente:"XXX000",
+    vendido: false,
+    }
+]
